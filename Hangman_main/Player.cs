d8aedd063f16566cf12bbf71d132f73b97c8a8a0
taperte <jana.taperte@gmail.com>
2021-01-08 04:10:33 +0200

    sākums + metodes + Player klase

---
+++ Hangman_main/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman_main
+{
+    class Player
+    {
+        public int IDnumber { get; set; }
+        public string Name { get; set; }
+        public int IncorrectGuessCount { get; set; }
+        public string[,] Hangman { get; set; }
+        public ConsoleColor Color { get; set; }
+
+
+        //This method prints a hangman image.
+        public void PrintHangmanImage()
+        {
+            Console.ForegroundColor = Color;
+            for (int row = 0; row < Hangman.GetLength(0); row++)
+            {
+                for (int column = 0; column < Hangman.GetLength(1); column++)
+                {
+                    Console.Write(Hangman[row, column]);
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+
+        public void PrintPlayerInfo()
+        {
+            Console.WriteLine("Player's info: ");
+            Console.WriteLine($"Name: {Name}.");
+            Console.WriteLine($"The number of incorrect guesses: {IncorrectGuessCount}.");
+            Console.WriteLine($"Player's color: {Color.ToString().ToLower()}.");
+            Console.WriteLine("Hangman image:");
+            PrintHangmanImage();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Hangman_main/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Hangman_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ENwords = File.ReadLines("C:\\Users\\''Dell''\\Desktop\\SGT_final_project\\WordListEN_cleaned.txt").ToList();
+            List<string> LVwords = File.ReadLines("C:\\Users\\''Dell''\\Desktop\\SGT_final_project\\WordListLV_cleaned.txt").ToList();
+            List<string> RUwords = File.ReadLines("C:\\Users\\''Dell''\\Desktop\\SGT_final_project\\WordListRU_cleaned.txt").ToList();
+            List<Player> players = new List<Player>();
+            int language, level, playerCount;
+            string english, latvian, russian;
+
+            //Change default output encoding to Unicode,
+            //so that Latvian text would be displayed correctly in console.
+            Console.OutputEncoding = Encoding.Unicode;
+
+            //1) Player(s) choose(s) language.
+            while (true)
+            {
+                Console.WriteLine("For English, enter 1.\nLai izvēlētos latviešu valodu, ievadiet 2.\nЧтобы выбрать русский язык, введите 3.");
+                string languageInput = Console.ReadLine();
+                if (string.IsNullOrEmpty(languageInput) || !int.TryParse(languageInput, out language))
+                {
+                    Console.WriteLine("Invalid input! / Kļūda! / Ошибка!");
+                    continue;
+                }
+                if (language < 1 || language > 3)
+                {
+                    Console.WriteLine("Invalid number! / Nepareizs skaitlis! / Неверное число!");
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //2) Info about the game.
+            english = "";
+            latvian = "";
+            russian = "";
+            Hangman_methods.SwitchLanguage(language, english, latvian, russian);
+            Console.WriteLine();
+
+            //3) Player(s) choose(s) level.
+            while (true)
+            {
+                english = "Please choose level (1, 2 or 3): ";
+                latvian = "Izvēlieties līmeni (1, 2 vai 3): ";
+                russian = "Выберите уровень (1, 2 или 3): ";
+                Hangman_methods.SwitchLanguage(language, english, latvian, russian);
+                string languageInput = Console.ReadLine();
+                if (string.IsNullOrEmpty(languageInput) || !int.TryParse(languageInput, out level))
+                {
+                    english = "Invalid input! "; latvian = "Kļūda! "; russian = "Ошибка! ";
+                    Hangman_methods.SwitchLanguage(language, english, latvian, russian);
+                    continue;
+                }
+                if (level < 1 || level > 3)
+                {
+                    english = "Invalid number! "; latvian = "Nepareizs skaitlis! "; russian = "Неверное число! ";
+                    Hangman_methods.SwitchLanguage(language, english, latvian, russian);
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //4) Number of players.
+            while (true)
+            {
+                english = "Please enter the number of players: ";
+                latvian = "Lūdzu, ievadiet spēlētāju skaitu: ";
+                russian = "Пожалуйста, введите количество игроков: ";
+                Hangman_methods.SwitchLanguage(language, english, latvian, russian);
+                string playerCountInput = Console.ReadLine();
+                if (string.IsNullOrEmpty(playerCountInput) || !int.TryParse(playerCountInput, out playerCount))
+                {
+                    english = "Invalid input! "; latvian = "Kļūda! "; russian = "Ошибка! ";
+                    Hangman_methods.SwitchLanguage(language, english, latvian, russian);
+                    continue;
+                }
+                if (playerCount < 1)
+                {
+                    english = "Invalid number! "; latvian = "Nepareizs skaitlis! "; russian = "Неверное число! ";
+                    Hangman_methods.SwitchLanguage(language, english, latvian, russian);
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            //5) The program creates player objects and saves them to the list.
+            Hangman_methods.CreatePlayers(players, playerCount, language, english, latvian, russian);
+
+            //Šo nevajadzēs, tas ir tikai testēšanai.
+            foreach (Player player in players)
+            {
+                player.PrintPlayerInfo();
+            }
+        }
+    }
+}
